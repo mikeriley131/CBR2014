0c0d71d1fe973211d1f9d87ce0f4559d1c903daa
Mike Riley <mikeriley131@gmail.com>
2014-06-08 13:49:36 -0400

    Reorganized folder structure

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// HEADER IMAGE CHANGE ON PRIMARY NAV ITEM HOVER
-$(document).ready(function() {
-	$('.nav-item').mouseenter(function() {
-	  var img = $(this).attr('data-headerimg'); 
-	  $('img#header-img').attr('src', img);
-	}).mouseleave(function() {
-	  $('img#header-img').attr('src', 'images/main-nav-image-default.jpg');
-	});
-});
