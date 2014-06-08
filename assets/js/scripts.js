3e445bf9704a46020b8eb76aaa38d11d7c864883
Mike Riley <mikeriley131@gmail.com>
2014-06-08 13:51:23 -0400

    Reorganized  folder structure

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// HEADER IMAGE CHANGE ON PRIMARY NAV ITEM HOVER
+$(document).ready(function() {
+	$('.nav-item').mouseenter(function() {
+	  var img = $(this).attr('data-headerimg'); 
+	  $('img#header-img').attr('src', img);
+	}).mouseleave(function() {
+	  $('img#header-img').attr('src', img);
+	});
+});
