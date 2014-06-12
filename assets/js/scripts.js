cb1fdf8fb12130201e708775ccb4bca806c2ea15
Mike Riley <mikeriley131@gmail.com>
2014-06-11 21:51:22 -0400

    Video link opens in new window and made header images links to respective pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 // HEADER IMAGE CHANGE ON PRIMARY NAV ITEM HOVER
 $(document).ready(function() {
 	$('.nav-item').mouseenter(function() {
-	  var img = $(this).attr('data-headerimg'); 
-	  $('img#header-img').attr('src', img);
+	  var img = $(this).attr('data-headerimg');
+	  var href = $(this).attr('data-headerlink');
+	  $('img#header-img').attr('src', img).parent().attr('href', href);
 	}).mouseleave(function() {
 	  var img = $(this).attr('data-headerimg');
 	  $('img#header-img').attr('src', img);
